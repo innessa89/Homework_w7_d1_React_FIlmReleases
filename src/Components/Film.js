6ef9edb6f0e427212d836929ab1deecc3554b23b
innessa89 <innessa89@gmail.com>
2021-11-22 23:53:17 +0000

    React MVP

---
+++ src/Components/Film.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Film=({name,url})=>{
+    return(
+    <a href={url}>{name}</a>
+    );
+}
+
+export default Film;
---
+++ src/Components/ReleaseCalendar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ReleaseCalendar=()=>{
+    return(
+        <button type="button" >
+            <a href="https://www.imdb.com/calendar/?region=gb">
+                View more upcoming release {'>>'}
+            </a>
+        </button>
+    )
+}
+
+export default ReleaseCalendar;
